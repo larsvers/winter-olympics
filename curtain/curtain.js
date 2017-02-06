db93717f3f49aa5e8e0a959749eae477105cc82b
larsvers <larsverspohl@gmail.com>
2017-02-06 09:05:04 +0100

    Curtain: rings now always calculate positions in accordance with official Olympic rings

--- curtain/curtain.js
+++ curtain/curtain.js
@@ -26,7 +26,7 @@ function dimCurtain(percent) {
 
 	d3.select('div#curtain').style('opacity', percent);
 
-}
+} // dim the curtain while scrolling
 
 function scrollHandler() {
 
@@ -34,8 +34,8 @@ function scrollHandler() {
   var scrollTop = document.body.scrollTop;
   var winHeight = window.innerHeight;
 
-  var progress = 1.5 - (scrollTop / winHeight);
-  if (progress > 1) progress = 1;
+  var progress = 1.25 - (scrollTop / winHeight);
+  if (progress > 1) progress = 1; // start dimming after 25% page-scroll
 
   dimCurtain(progress);
 
@@ -50,6 +50,7 @@ function scrollHandler() {
 
 } // scrollHandler()
 
+
 document.addEventListener('scroll', scrollHandler);
 
 
@@ -61,37 +62,92 @@ var data, nodes = {};
 var width = window.innerWidth, height = window.innerHeight;
 var simulation;
 
-var margin = 20;
 
-// position variables
+// === Olympic ring position calculations === //
 
-var clusterN = 5;
-var n = 200;
-var ringRadius = 100;
+var ringCalculations = function() {
 
-var center = {}; // object to hold different circle-centers
+	// --- Algorithm to calculate Olympic ring positions and sizes --- //
 
-center.olympic = {
-	0: [(width-2*margin)*0.21+margin, (height-2*margin)*0.415+margin], 
-	1: [(width-2*margin)*0.5+margin, (height-2*margin)*0.415+margin],
-	2: [(width-2*margin)*0.79+margin, (height-2*margin)*0.415+margin],
-	3: [(width-2*margin)*0.355+margin, (height-2*margin)*0.585+margin],
-	4: [(width-2*margin)*0.645+margin, (height-2*margin)*0.585+margin]
-};
+	// height/width ratio = 0.45 (measured from Olympic rings)
+	// radius = height / 3
 
-center.start = {
-	0: [(width-2*margin)*0.21+margin, -2*ringRadius],
-	1: [(width-2*margin)*0.5+margin, -2*ringRadius],
-	2: [(width-2*margin)*0.79+margin, -2*ringRadius],
-	3: [(width-2*margin)*0.355+margin, -2*ringRadius],
-	4: [(width-2*margin)*0.645+margin, -2*ringRadius]
-};
+	// 1. Calculate Olympic ring area width = width/2
+	// 2. Calculate height =  width * 0.45
+	// 3. Calculate radius = height / 3
+	// 4. Establish area orign = 0 + window.innerWidth / 4
+
+	// 5. Calculate top left ring center: x = origin + radius, y = origin + radius
+	// 6. Caluclate top middle ringe center: x = origin + area width/2, y = origin + radius
+	// 7. Calculate top right ring center: x = origin + area width - radius, y = origin + radius
+
+	// 8. Calculate ring distance = top middle center x - top left center x
+
+	// 9. Calculate bottom left ring center: x = origin + radius + (ring distance)/2, y = area height - radius 
+	// 10. Calculate bottom right ring center: x = origin + area width/2 + (ring distance)/2, y = area height - radius 
+
+	// Result object
+
+	var result = {};
+
+	// Position variables
+
+	var margin = 20;
+
+	result.clusterN = 5;
+	result.n = 200;
+
+
+	// Ring area
+
+	var origin = {};
+
+	var areaWidth = window.innerWidth * 0.75 > 750 ? 750 : window.innerWidth * 0.75; // change to change overall ring area size
+	var areaHeight = areaWidth * 0.45;
+	result.radius = areaHeight / 3;
+	origin.x = (window.innerWidth - areaWidth)/2; // change to change position
+	origin.y = (window.innerHeight - areaHeight)/2;
+
+	// Ring centers
+
+	var rings = { topLeft: {}, topMiddle: {}, topRight: {}, bottomLeft: {}, bottomRight: {} };
+
+	rings.topLeft.x = origin.x + result.radius;
+	rings.topLeft.y = origin.y + result.radius;
+	rings.topMiddle.x = origin.x + areaWidth/2;
+	rings.topMiddle.y = origin.y + result.radius;
+	rings.topRight.x = origin.x + areaWidth - result.radius;
+	rings.topRight.y = origin.y + result.radius;
+
+	var ringDistance = rings.topMiddle.x - rings.topLeft.x;
+
+	rings.bottomLeft.x = origin.x + areaWidth/2 - ringDistance/2;
+	rings.bottomLeft.y = origin.y + areaHeight - result.radius;
+	rings.bottomRight.x = origin.x + areaWidth/2 + ringDistance/2;
+	rings.bottomRight.y = origin.y + areaHeight - result.radius;
+
+	result.olympic = {
+		0: [rings.topLeft.x, rings.topLeft.y],
+		1: [rings.topMiddle.x, rings.topMiddle.y],
+		2: [rings.topRight.x, rings.topRight.y],
+		3: [rings.bottomLeft.x, rings.bottomLeft.y],
+		4: [rings.bottomRight.x, rings.bottomRight.y]
+	};
+
+	result.start = {
+		0: [rings.topLeft.x, 0],
+		1: [rings.topMiddle.x, 0],
+		2: [rings.topRight.x, 0],
+		3: [rings.bottomLeft.x, 0],
+		4: [rings.bottomRight.x, 0]
+	};
+
+	return result;
+
+}; // ringCalculations()
 
 
 // === Set up canvas and elements === //
-
-// d3.select('#curtain .top').style('width', width + 'px').style('height', height + 'px');
-// d3.select('#text-wrap').style('width', width + 'px').style('height', height + 'px');
 
 var canvas = d3.select('#curtain .top').append('canvas').attr('id', 'curtain').attr('width', width).attr('height', height);
 
@@ -133,9 +189,8 @@ function getData(clusterNumber, nodeNumber, clusterCenter, circleRadius) {
 
 // === databind options === //
 
-
 function databind(data) {
-	
+
 	var join = custom.selectAll('custom.circles')
 		.data(data);
 
@@ -145,13 +200,12 @@ function databind(data) {
 		.attr('cx', function(d) { return d.x; })
 		.attr('cy', -10)
 		.attr('r', function(d) { return d.radius; })
-		// .attr('fillStyle', '#ADCEFF');
 		.attr('fillStyle', '#5892A9');
 
 	join
 		.merge(enterSel)
 		.transition().duration(5000)
-		.delay(function(d,i) { return (Math.random()*i) / n * 1000; })
+		.delay(function(d,i) { return (Math.random()*i) / nodes.pos.n * 1000; })
 		.attr('fillStyle', function(d) { return d.colour; })
 		.attr('cy', function(d) { return d.y; });
 
@@ -165,7 +219,7 @@ function databind(data) {
 
 
 function draw() {
-	
+
 	context.clearRect(0, 0, width, height);
 
 	var elements = custom.selectAll('custom.circles');
@@ -173,8 +227,6 @@ function draw() {
 	elements.each(function(d,i) {
 
 		var node = d3.select(this);
-
-		// log(node.attr('cx'), node.attr('cy'), node.attr('r'), node.attr('fillStyle'));
 
 		context.beginPath();
 		context.moveTo(node.attr('cx') + node.attr('r'), node.attr('cy'));
@@ -248,9 +300,9 @@ function drawNode(d) {
 
 // === Calls === //
 
-nodes.center = getData(clusterN, n, center.olympic, ringRadius);
+nodes.pos = ringCalculations();
 
-// log(nodes.center);
+nodes.center = getData(nodes.pos.clusterN, nodes.pos.n, nodes.pos.olympic, nodes.pos.radius);
 
 databind(nodes.center);
 
